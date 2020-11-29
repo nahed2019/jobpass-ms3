f211f857b5f56c6986668d36ce17c4eaa46bfe09
nahed2019 <nanawalid@gmail.com>
2020-11-29 23:08:37 +0000

    add to-top-btn to the project,style shop.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,31 @@
-$(".datepicker").datepicker({
-    format: "dd/mm/yyyy",
-    clearBtn: true,
-  });
+/**
+ * CREDIT: code for floating buttons taken from 
+ * https://www.w3schools.com/howto/howto_js_scroll_to_top.asp 
+ */
+window.onscroll = function () {
+    scrollFunction();
+};
 
+
+/**
+ * makes floating button for go to top visible once user starts scrolling.
+ */
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $("#to-top-btn").addClass('active');
+    } else {
+        $("#to-top-btn").removeClass('active');
+    }
+}
+
+$('#to-top-btn').click(function () {
+    topFunction();
+});
+
+/**
+ * Scrolls the user bACK to the top of the page
+ */
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
